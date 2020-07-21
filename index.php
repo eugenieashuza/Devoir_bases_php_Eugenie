38e970122c8990c4e4327db64e1ee7fb8d15587f
eugenie ashuza <eugenieashuza@gmail.com>
2020-07-21 17:04:13 +0200

    creation de la fonction somme

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
-//les variables
+//1.les variables
 $nom=eugenie;
 $prenom=danais;
 //affichage des variables
 echo $nom;
 echo $prenom;
 
+//2.fonction
 
+ function somme($nb1,$nb2)
+{
+    # code...
+     $som=$nb1+$nb2;
+     return $som;
+    
+}
+$som=
 
